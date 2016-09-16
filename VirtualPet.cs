da81ebe62d310d1c9bfe214c86226f348d0ef43b
Kendra Egert <Kendra Egert>
2016-09-16 19:53:53 -0400

    first commit

---
+++ VirtualPet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVirturalPet
+{
+    class VirtualPet
+    {
+        private int petBathroom;
+        private int petThirst;
+        private int petHungry;
+        private int petBored;
+
+
+        //contructors
+        public VirtualPet(int bathroom, int thirst, int hungry, int bored)
+        {
+            this.petBathroom = bathroom;
+            this.petThirst = thirst;
+            this.petHungry = hungry;
+            this.petBored = bored;
+
+             }
+
+
+
+
+
+
+
+
+
+
+    }
+}
