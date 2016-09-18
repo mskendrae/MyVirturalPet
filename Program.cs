8e21c40a3b4ebf6e8f5e832ae340e2076c8ebdcc
Kendra Egert <Kendra Egert>
2016-09-18 11:14:40 -0400

    menu and method changes

--- Program.cs
+++ Program.cs
@@ -10,6 +10,57 @@ namespace MyVirturalPet
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+            //User Greeting 
+            Console.WriteLine("Welcome to Virtural Panda Pet!");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("   ,,,         ,,,");
+            Console.WriteLine(" ; \"   ^;     ;'   \" ; ");
+            Console.WriteLine("      ; s$$$$$$$s;   ");
+            Console.WriteLine("   ,  s$$$$$$$$$$s  ,'");
+            Console.WriteLine("   ; s$$$$$$$$$$$$$$$   ");
+            Console.WriteLine("    $$$$$$$$$$$$$$$$$   ");
+            Console.WriteLine("   $$$$P\"\"Y$$$Y\"\"W$$$$$   ");
+            Console.WriteLine("   $$$$  p\"$$$\"q  $$$$$  ");
+            Console.WriteLine("    $$$$  .$$$$$.  $$$$    ");
+            Console.WriteLine("     $$$$$$$$$$$$$$$$$     ");
+            Console.WriteLine("     \"Y$$$\" * \"$$$Y\"   ");
+            Console.WriteLine("         \"$$$$$\"   ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Please name your panda:");
+            string pandaName = Console.ReadLine();
+
+            //Display Status of pet
+            VirtualPet statusPet = new VirtualPet();
+           
+
+
+            Console.WriteLine("Please keep an eye on the health status of " + pandaName + " choose from the menu ways to care for your panda.");
+
+            Console.WriteLine("Hunger " + statusPet );
+
+           //UserInstructions to care for the Panda and accept user input
+              Console.WriteLine("Please enter a number");
+              Console.WriteLine("1. Give panda Bamboo?");
+              Console.WriteLine("2. Give panda water?");
+              Console.WriteLine("3. Let pet out to poop?");
+              Console.WriteLine("4. Play with your panda outside?");
+              int userChoice = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+        } //end main
+    } //end class
+} //end namespace
--- VirtualPet.cs
+++ VirtualPet.cs
@@ -9,56 +9,107 @@ namespace MyVirturalPet
     class VirtualPet
     {
         //fields
-        private int petBathroom;
+        private int petPoop;
         private int petThirst;
-        private int petHungry;
+        private int petHungry ;
         private int petBored;
-
+      //  private int petStatus;
 
         //properties
 
-       public int Bathroom
-             {
-               get {return this.petBathroom;}
-               set {this.petBathroom = value;}
-             }
+        public int Poop
+        {
+            get { return this.petPoop; }
+            set { this.petPoop = 10; }
+        }
 
         public int Thirst
-             {
-                get {return this.petThirst;}
-                set {this.petThirst = value;}
-             }
+        {
+            get { return this.petThirst; }
+            set { this.petThirst = 7; }
+        }
 
-        public int Hungry
-             {
-                get {return this.petHungry;}
-                set {this.petHungry = value;}
-             }
+        public int Hunger
+        {
+            get { return this.petHungry; }
+            set { this.petHungry = 8; }
+        }
 
-         public int Bored
-             {
-                get {return this.petBored;}
-                set {this.petBored = value;}
-             }
+        public int Bored
+        {
+            get { return this.petBored; }
+            set { this.petBored = 5; }
+        }
 
+      //  public int Status
+      // {
+      //      get { return this.petStatus; }
+      //      set { this.petStatus = value; }
+    //    }
+
+       
+        //methods
+
+        public int feedPet()
+           {
+            petPoop ++;
+            petThirst ++;
+            petHungry -= 3;
+            petBored +=2;
+
+             return petHungry; 
+            }
+
+        public int waterPet()
+          {
+            petPoop += 2;
+            petThirst-=3;
+            petHungry += 1;
+            petBored += 3;
+
+            return petThirst;
+          }
+
+        public int walkPet()
+          {
+            petPoop -= 4;
+            petThirst += 2;
+            petHungry += 2;
+            petBored -= 3;
+
+            return petPoop;
+          }
+
+
+        public int playBall()
+        {
+            petPoop -= 2;
+            petThirst += 2;
+            petHungry += 2;
+            petBored -= 4;
+
+            return petBored;
+        }
 
         //contructor
-        public VirtualPet(int petBathroom, int petThirst, int petHungry, int petBored)
-            {
-            this.petBathroom = petBathroom;
+        public VirtualPet()
+        {
+
+        }
+
+        public VirtualPet(int petPoop, int petThirst, int petHungry, int petBored)
+        {
+            this.petPoop = petPoop;
             this.petThirst = petThirst;
             this.petHungry = petHungry;
             this.petBored = petBored;
-            }
+        }
 
 
 
 
+    }// end class
 
 
+    }//end namespace
 
-
-
-
-    }
-}
